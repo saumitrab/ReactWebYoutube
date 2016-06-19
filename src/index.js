a8df04602136a0ef8c4561a41105cc105aef10e0
Saumitra Bhanage <bhanage.saumitra@gmail.com>
2016-06-19 15:53:09 -0700

    Add videolist and videolistitem

--- src/index.js
+++ src/index.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
-
+import VideoList from './components/videolist';
 const API_KEY = 'AIzaSyCnaELaoOiLo_OlZd_6C7wETggvK7OYr3E';
 
-const App = () => {
-  return (<div>
-      <SearchBar />
-    </div>);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    };
+  }
+  componentWillMount() {
+    YTSearch({key: API_KEY, term: 'puppy'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+  }
 }
 
 
